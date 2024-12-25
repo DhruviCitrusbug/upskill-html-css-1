860a7ef03c8d419ae260c431c5b767c0bd976b3a
DhruviCitrusbug <dhruvi.citrusbug@gmail.com>
2024-12-25 22:32:23 +0530

    HTML-CSS design implementation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,16 @@
 $(document).ready(function () {
     $(".faq-card").click(function () {
+        let answerSection = $(this).find(".faq-answer");
+        let button = $(this).find("#faq-btn");
+        let icon = button.find("img");
         if ($(this).hasClass("faq-card-active")) {
             $(this).removeClass("faq-card-active");
-            $(this).findClass("faq-answer").hide();
+            answerSection.slideUp();
+            icon.css("transform", "rotate(90deg)");
         }
         else {
             $(this).addClass("faq-card-active");
+            answerSection.slideDown();
         }
     });
 });
